3f9dc96de96b3f3eed88ae45ac4d5b2072499586
Somil Agrawal <somilagrawal680@gmail.com>
2022-09-23 20:38:08 +0530

    update. newsitem.js

--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -19,7 +19,7 @@ const NewsItem = (props)=> {
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
                         <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} <br /> {new Date(date).toGMTString()}</small></p>
-                        <div class="d-flex justify-content-center">
+                        <div className="d-flex justify-content-center">
                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                         </div>
                     </div>
